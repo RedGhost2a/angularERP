dddb8acc3b17766872489548c7f4965cd2b411e7
red <red@test.com>
2023-04-13 10:53:13 +0200

    [ADD]user list

--- src/app/parametres/user-list/user-list.component.ts
+++ src/app/parametres/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {UserService} from "../../_service/user.service";
 import {User} from "../../_models/users";
-import {Buffer} from 'buffer';
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-user-list',
@@ -12,7 +12,8 @@ export class UserListComponent implements OnInit {
 
   @Input() user!: User;
   @Output() deleteUser: EventEmitter<any> = new EventEmitter()
-
+  displayedColumns: string[] = ['id', 'title', 'firstName', 'lastName', 'email', 'role', 'boutons'];
+  dataSource!: any;
   listUser !: User[];
 
 
@@ -30,14 +31,15 @@ export class UserListComponent implements OnInit {
 
   getAll(): void {
     this.userService.getAll().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
       // Convert each user's avatar data from a Buffer to a base64-encoded string
       this.listUser = data.map((user: { avatarUrl: any; }) => {
         // Check if avatarUrl is null or undefined
-        if (user.avatarUrl && user.avatarUrl.data) {
-          // Convert avatarUrl.data to a Buffer object and then to a base64-encoded string
-          const avatarUrlString = Buffer.from(user.avatarUrl.data).toString('base64');
-          user.avatarUrl = 'data:image/jpeg;base64,' + avatarUrlString;
-        }
+        // if (user.avatarUrl && user.avatarUrl.data) {
+        //   // Convert avatarUrl.data to a Buffer object and then to a base64-encoded string
+        //   const avatarUrlString = Buffer.from(user.avatarUrl.data).toString('base64');
+        //   user.avatarUrl = 'data:image/jpeg;base64,' + avatarUrlString;
+        // }
         console.log(user)
         return user;
       });
