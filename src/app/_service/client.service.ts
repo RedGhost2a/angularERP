a1f85c1a7269dd1cd818568f6e4137050642ffee
aubri <aubrii@hotmail.fr>
2023-06-01 11:47:44 +0200

    [ADD] Ajout d'un tableau de clients en attribut, ajout de fonction pour la creation d'un client

--- src/app/_service/client.service.ts
+++ src/app/_service/client.service.ts
@@ -3,18 +3,23 @@ import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from '../../environments/environment';
+import {Client} from "../_models/client";
+import {EditComponent} from "../clients/edit/edit.component";
+import {MatDialog} from "@angular/material/dialog";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  clients :Client [] = [];
 
-  constructor(private router: Router,
-              private http: HttpClient) {
+
+  constructor(private router: Router, private http: HttpClient, private dialog: MatDialog) {
   }
 
   register(client: any): Observable<any> {
+    this.clients = []
     return this.http.post(`${environment.apiUrl}/clients/new`, client);
   }
 
@@ -22,7 +27,8 @@ export class ClientService {
     return this.http.get(`${environment.apiUrl}/clients/${id}/`);
   }
 
-  getAllByEntreprise(entrepriseId:number){
+  getAllByEntreprise(entrepriseId:number): Observable<any>{
+    this.clients = [] ;
     return this.http.get(`${environment.apiUrl}/clients/entreprise`, {
       params: {
         EntrepriseId: entrepriseId
@@ -39,11 +45,13 @@ export class ClientService {
 
 
   getAll(): Observable<any> {
+    this.clients = [];
     return this.http.get(`${environment.apiUrl}/clients`)
 
   }
 
   update(client: any, id: number): Observable<any> {
+    this.clients = [];
     return this.http.put(`${environment.apiUrl}/clients/${id}`, client)
   }
 
@@ -52,7 +60,18 @@ export class ClientService {
   }
 
   deleteByID(id: any): Observable<any> {
+    this.clients = [];
     return this.http.delete(`${environment.apiUrl}/clients/${id}`)
+  }
+  openDialogCreateClient(client: Client | null, disable: boolean, refreshData:any) {
+    this.dialog.open(EditComponent, {
+      data: [client, disable],
+      width: '90%',
+      height: '80%'
+    }).afterClosed().subscribe(async result => {
+      refreshData()
+
+    });
   }
 
 }
