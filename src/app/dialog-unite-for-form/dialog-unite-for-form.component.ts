3cb6150b3894cbb6f098f1acd1685faa3b776698
aubri <aubrii@hotmail.fr>
2023-07-11 14:05:57 +0200

    [ADD] Add input EntrepriseId to enable user to create unités for these entreprise

--- src/app/dialog-unite-for-form/dialog-unite-for-form.component.ts
+++ src/app/dialog-unite-for-form/dialog-unite-for-form.component.ts
@@ -8,6 +8,7 @@ import {UniteForForm} from "../_models/uniteForForm";
 import {TypeCoutService} from "../_service/typeCout.service";
 import {TypeCout} from "../_models/type-cout";
 import {DataSharingService} from "../_service/data-sharing-service.service";
+import {Entreprise} from "../_models/entreprise";
 
 @Component({
   selector: 'app-dialog-unite-for-form',
@@ -24,7 +25,7 @@ export class DialogUniteForFormComponent implements OnInit {
   typeCout:any[]=[]
   categories:any[]=[]
   uniteListTypeCategorie!: any[];
-
+  listEntreprise : Entreprise [] = []
   constructor(private uniteForFormService:UniteForFormService,
               private typeCoutService: TypeCoutService,
               private form:FormBuilder,
@@ -53,16 +54,26 @@ export class DialogUniteForFormComponent implements OnInit {
 
 getId(): void {
   this.userService.getById(this.user).subscribe(data=>{
-    this.entrepriseId=data.Entreprises[0].id
-    this.uniteForForm.controls['EntrepriseId'].setValue(this.entrepriseId);
-    console.log(this.entrepriseId)
-    this.getAllTypeCouts(this.entrepriseId)
+    data.Entreprises.forEach((entreprise: Entreprise) =>{
+      this.listEntreprise.push(entreprise)
+    })
+    // this.entrepriseId=data.Entreprises[0].id
+    // this.uniteForForm.controls['EntrepriseId'].setValue(this.entrepriseId);
+    // console.log(this.entrepriseId)
+    // this.getAllTypeCouts(this.entrepriseId)
     // this.typeCoutService.getAllTypeCouts(this.entrepriseId).subscribe(data => {
     //   this.type = data.map(item => item.type);
     //   this.categorie = data.map(item => item.categorie);
     //   console.log(this.type, this.categorie);
     // });
   })
+
+}
+getEntreprise(){
+    // this.uniteForForm.controls['EntrepriseId'].setValue(2)
+  console.log('form', this.uniteForForm.getRawValue())
+  this.getAllTypeCouts(this.uniteForForm.controls['EntrepriseId'].value)
+  console.log("unite for form controle entreprise ",this.uniteForForm.controls['EntrepriseId'].value)
 
 }
 
@@ -83,9 +94,10 @@ getId(): void {
   }
   //Recupere tous les type de couts pour implementer le select picker du template
   getAllTypeCouts(entrepriseID: number): void {
+    console.log('entreprise get all tyupe couts' , entrepriseID)
     this.typeCoutService.getAllTypeCouts(entrepriseID).subscribe(data => {
         this.typeCout = data;
-        console.log(this.typeCout)
+        console.log('type cout',this.typeCout)
 
       }
     )
@@ -102,7 +114,7 @@ getId(): void {
     this.typeCoutService.getCategorieByType(type).subscribe(data => {
       this.categories = data;
 
-      console.log(this.categories);
+      console.log("categorie",this.categories);
     });
   }
 
